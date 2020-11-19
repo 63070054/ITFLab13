af02a46707a9236916eee98f584bbb02e15085bf
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 18:07:26 +0700

    Update show.php

--- show.php
+++ show.php
@@ -17,21 +17,21 @@ if (mysqli_connect_errno($conn))
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <div class="container">
-<table width="600" class="table table-bordered table table-hover ">
-  <thead class="thead-dark">
-    <tr>
-      <th width="100"> <div align="center">Name</div></th>
-      <th width="350"> <div align="center">Comment </div></th>
-      <th width="150"> <div align="center">Link </div></th>
-      <th width="200"> <div align="center">Button </div></th>
-    </tr>
-  </thead>
-</table>
-<center>
-  <a href="form.html">
-    <button type="submit" class="btn btn-primary" align="center">Add</button>
-  </a>
-</center>
+  <table width="600" class="table table-bordered table table-hover ">
+    <thead class="thead-dark">
+      <tr>
+        <th width="100"> <div align="center">Name</div></th>
+        <th width="350"> <div align="center">Comment </div></th>
+        <th width="150"> <div align="center">Link </div></th>
+        <th width="200"> <div align="center">Button </div></th>
+      </tr>
+    </thead>
+  </table>
+  <center>
+    <a href="form.html">
+      <button type="submit" class="btn btn-primary" align="center">Add</button>
+    </a>
+  </center>
 </div>
 <?php
 while($Result = mysqli_fetch_array($res))
