65c82c2d16c82a4c3467619dddc72b897f816885
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 18:34:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
     $conn = mysqli_real_connect($conn, '63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
-    $sql = 'DELETE FROM user WHERE ID = '.$_GET['ID'].'';
+    $sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['id'].'';
     if(mysqli_query($conn, $sql)) {
         echo "DELETE COMPLETED";
     }
