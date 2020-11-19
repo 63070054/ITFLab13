bd14b05e22c3277478878552f383b8e6ab05eb9d
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 20:08:49 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
     $conn = mysqli_connect('63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
-    $sql = 'SELECT * FROM user WHERE ID = '.$_GET['ID'].'';
+    $sql = 'SELECT * FROM guestbook WHERE ID = '.$_GET['ID'].'';
     $query = mysqli_query($conn, $sql);
     if(!$query) {
         header('Location: show.php');
