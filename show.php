e2aadfde20c50a4847ce632cd1de287a469df2ce
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 17:20:50 +0700

    Add files via upload

---
+++ show.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, '63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" class="table table-bordered table table-hover ">
+  <thead class="thead-dark">
+    <tr>
+      <th width="100"> <div align="center">Name</div></th>
+      <th width="350"> <div align="center">Comment </div></th>
+      <th width="150"> <div align="center">Link </div></th>
+    </tr>
+  </thead>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td><?php echo $Result['Link'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+</body>
+</html>
