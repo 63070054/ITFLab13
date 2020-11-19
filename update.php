598cab5abffc600483f9f01f6bdecbfedd1a7095
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 22:42:15 +0700

    Update update.php

--- update.php
+++ update.php
@@ -14,31 +14,4 @@
         echo "FAILED TO EDIT";
     }
 ?>
-<?php
-
-$conn = mysqli_connect('63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-
-$name = $_POST['Name'];
-$comment = $_POST['Comment'];
-$link = $_POST['Link'];
-
-
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } 
-  else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-
-  
-mysqli_close($conn);
-?>
 <a href="show.php" class="btn btn-primary">BACK</a>
