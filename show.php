6a57bdd823f698aa5c038214cecd42e33d6c1eda
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 19:30:20 +0700

    Update show.php

--- show.php
+++ show.php
@@ -33,7 +33,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    <td>
+    <td align="center">
       <a href="delete.php?ID=<?php echo $Result['ID'];?>" class="btn btn-primary">DEL</a>
       <a href="edit.php?ID=<?php echo $Result['ID'];?>" class="btn btn-primary">EDIT</a>
     </td>
