4a2a37f761c20909830582ac894d8d82d4d155ee
63070054 <73682760+63070054@users.noreply.github.com>
2020-11-19 22:36:11 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,10 +1,16 @@
 <?php
 	$conn = mysqli_connect('63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
 
-	$name = $_POST['name'];
-	$comment = $_POST['comment'];
-	$link = $_POST['link'];
-	$id = $_POST['id'];
+	$name = $_POST['Name'];
+	$comment = $_POST['Comment'];
+	$link = $_POST['Link'];
+	$id = $_POST['ID'];
 
-	$sql = 'UPDATE guestbook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
+    $sql = 'UPDATE guestbook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
+    if(mysqli_query($conn, $sql)) {
+        echo "EDIT COMPLETED";
+    }
+    else {
+        echo "FAILED TO EDIT";
+    }
 ?>
